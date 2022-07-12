4d1c658df68c88848752958cd57d6e6ec0464147
Yaroslav Oliinyk <4106677@gmail.com>
2022-07-12 22:59:13 +0300

    HW-08

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,16 +1,16 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector('.menu-open-btn'),
-    closeMenuBtn: document.querySelector('.close-open-btn'),
-    menu: document.querySelector('.mob-menu'),
-    body: document.querySelector('body'),
-  };
+	const refs = {
+		openMenuBtn: document.querySelector(".menu-open-btn"),
+		closeMenuBtn: document.querySelector(".menu__mob-close"),
+		menu: document.querySelector(".menu__mob"),
+		body: document.querySelector("body"),
+	};
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no-scroll');
-  }
+	function toggleMenu() {
+		refs.menu.classList.toggle("is-hidden");
+		refs.body.classList.toggle("no-scroll");
+	}
 })();
